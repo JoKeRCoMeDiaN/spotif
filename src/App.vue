03a6013495dd69d757f88674aefe33e2f0001484
JoKeRCoMeDiaN <52279858+JoKeRCoMeDiaN@users.noreply.github.com>
2020-10-10 02:53:53 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
         <section class="playlist">
           <h3>The Playlist</h3>
           <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing' : 'song'">
-            {{ song.title }} = {{song.artist}}
+            {{ song.title }} - {{song.artist}}
           </button>
           </section>   
     </main>  
